d9aecd8d0b5bf46e6787c2b80d1ff2bacb991bb7
Guilherme Dias <guidias200220@gmail.com>
2024-10-21 17:51:19 -0300

    feat: add AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import './App.css'
 import RegisterForm from './components/RegisterForm'
 import LoginForm from './components/LoginForm';
 import Dashboard from './components/Dashboard';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './context/ProtectedRoute';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/register" element={<RegisterForm/>}/>
-          <Route path="/login" element={<LoginForm/>}/>
-          <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<RegisterForm/>}/>
+            <Route path="/login" element={<LoginForm/>}/>
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
